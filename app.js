55b31249c9c5f148c2f60aed080ccaa02f50c34b
MauroGra <maurograndis30@gmail.com>
2021-12-09 16:29:50 -0300

    proyecto

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app =  express();
+const path = require('path');
+const { allowedNodeEnvironmentFlags } = require("process");
+
+app.set("port",process.env.PORT || 3020);
+
+app.listen(app.get("port"),()=> console.log ("servidor corriendo"))
+
+app.get("/",(req,res)=>{res.sendFile(path.resolve(__dirname,"./views", "index.html"))})  
+
+app.use(express.static(path.resolve(__dirname,"./public")));
+
+app.get("/register",(req,res)=>{res.sendFile(path.resolve(__dirname,"./views", "register.html"))})  
+
+
+app.get("/login",(req,res)=>{res.sendFile(path.resolve(__dirname,"./views", "login.html"))})  
